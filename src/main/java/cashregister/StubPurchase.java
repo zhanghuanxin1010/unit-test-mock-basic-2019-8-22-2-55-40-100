b6f33fe0adeee05fc442a91972060429f7618075
unknown <1012151538@qq.comgit config --global user.name >
2019-08-26 18:03:31 +0800

    3

---
+++ src/main/java/cashregister/StubPurchase.java
@@ -0,0 +1,6 @@
+package cashregister;
+
+public class StubPurchase {
+
+	
+}
--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -4,6 +4,8 @@ package cashregister;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.exceptions.base.MockitoInitializationException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -47,18 +49,30 @@ public class CashRegisterTest {
     	assertEquals("test product\t1.0\n", mockPrinter.getTempText());
     }*/
 
-    @Test
+  /*  @Test
     public void should_print_the_stub_purchase_when_call_process() {
-        //given
-        //when
+    	//given
+    	StubPurchase stubPurchase=new StubPurchase();
+      //when
+    	
+    	cashRegister.process(stubPurchase);
         //then
-    }
+    	assertEquals("test product\t1.0\n", mockPrinter.getTempText());
+
+    	
+    }*/
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
         //given
+    	Printer printer=Mockito.mock(Printer.class);
+    	Purchase purchase=Mockito.mock(Purchase.class);
+    	Mockito.when(purchase.asString()).thenReturn("test product"); 	
         //when
+    	CashRegister cashRegister=new CashRegister(printer);
+    	cashRegister.process(purchase);
         //then
+    	Mockito.verify(printer).print("test product");
     }
 
 }
